124094f5e1f35de0e4af4e6b4bcfb7b010619c66
SlawekHupka <slawek@hupka.pl>
2023-01-10 14:33:08 +0100

    refactoring js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
+{
+     const toogleBackground = () => {
+          const body = document.querySelector(".body");
+          const colorDark = document.querySelector(".js-colorDark");
 
-let buttonPhotoRemove = document.querySelector(".js-buttonPhotoRemove");
-let photoSlide = document.querySelector(".js-photoSlide");
-let buttonChange = document.querySelector(".js-buttonChange");
+          body.classList.toggle("body--dark");
+          colorDark.innerText = body.classList.contains("body--dark") ? "na jasny" : "na ciemny";
+     };
 
-buttonPhotoRemove.addEventListener("click", () => {
-     photoSlide.classList.toggle("display");
-     buttonChange.innerText = photoSlide.classList.contains("display") ? "Pokaż" : "Ukryj";
-});
+     const onPhotoClic = () => {
+          const buttonChange = document.querySelector(".js-buttonChange");
+          const photoSlide = document.querySelector(".js-photoSlide");
 
-let sechangeBackgroundButton = document.querySelector(".js-sechangeBackgroundButton");
-let body = document.querySelector(".body");
-let colorDark = document.querySelector(".js-colorDark");
+          photoSlide.classList.toggle("display");
+          buttonChange.innerText = photoSlide.classList.contains("display") ? "Pokaż" : "Ukryj";
+     };
 
-sechangeBackgroundButton.addEventListener("click", () => {
-     body.classList.toggle("body--dark");
-     colorDark.innerText = body.classList.contains("body--dark") ? "na jasny" : "na ciemny";
-});
+     const init = () => {
+          const sechangeBackgroundButton = document.querySelector(".js-sechangeBackgroundButton");
+          const buttonPhotoRemove = document.querySelector(".js-buttonPhotoRemove");
+
+          sechangeBackgroundButton.addEventListener("click", toogleBackground);
+          buttonPhotoRemove.addEventListener("click", onPhotoClic);
+     };
+
+     init();
+}
